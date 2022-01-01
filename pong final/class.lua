6c94c3900beec793ef25b1935223bc57fef95fc7
ShounakDua <96842886+ShounakDua@users.noreply.github.com>
2022-01-02 03:09:46 +0530

    Update class.lua

--- pong final/class.lua
+++ pong final/class.lua
@@ -10,7 +10,7 @@ local function include_helper(to, from, seen)
 
 	seen[from] = to
 	for k,v in pairs(from) do
-		k = include_helper({}, k, seen) -- keys might also be tables
+		k = include_helper({}, k, seen) 
 		if to[k] == nil then
 			to[k] = include_helper({}, v, seen)
 		end
@@ -18,20 +18,19 @@ local function include_helper(to, from, seen)
 	return to
 end
 
--- deeply copies `other' into `class'. keys in `other' that are already
--- defined in `class' are omitted
+
 local function include(class, other)
 	return include_helper(class, other, {})
 end
 
--- returns a deep copy of `other'
+
 local function clone(other)
 	return setmetatable(include({}, other), getmetatable(other))
 end
 
 local function new(class)
-	-- mixins
-	class = class or {}  -- class can be nil
+	
+	class = class or {}  
 	local inc = class.__includes or {}
 	if getmetatable(inc) then inc = {inc} end
 
@@ -42,13 +41,13 @@ local function new(class)
 		include(class, other)
 	end
 
-	-- class implementation
+	
 	class.__index = class
 	class.init    = class.init    or class[1] or function() end
 	class.include = class.include or include
 	class.clone   = class.clone   or clone
 
-	-- constructor call
+	
 	return setmetatable(class, {__call = function(c, ...)
 		local o = setmetatable({}, c)
 		o:init(...)
@@ -56,7 +55,7 @@ local function new(class)
 	end})
 end
 
--- interface for cross class-system compatibility (see https://github.com/bartbes/Class-Commons).
+
 if class_commons ~= false and not common then
 	common = {}
 	function common.class(name, prototype, parent)
@@ -68,6 +67,6 @@ if class_commons ~= false and not common then
 end
 
 
--- the module
+
 return setmetatable({new = new, include = include, clone = clone},
 	{__call = function(_,...) return new(...) end})
